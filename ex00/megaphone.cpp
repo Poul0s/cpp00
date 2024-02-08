76316ba990f00f60513a6444968370303a08d949
Paul Salame <psalame@2B6.42angouleme.fr>
2024-02-08 13:09:48 +0100

    ex00: fixed toupper approch + added no parameter comportement

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,24 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
+/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:44:56 by psalame           #+#    #+#             */
-/*   Updated: 2023/12/11 17:45:43 by psalame          ###   ########.fr       */
+/*   Updated: 2024/02/08 13:08:59 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-void	ft_printupper(char *str)
+void	ft_printupper(std::string str)
 {
 	int	i;
 
 	for (i = 0; str[i] != 0; i++)
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - ' ';
-	}
+		str[i] = toupper(str[i]);
 	std::cout << str;
 }
 
@@ -28,6 +26,8 @@ int	main(int ac, char **av)
 {
 	int	i;
 
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (i = 1; i < ac; i++) 
 		ft_printupper(av[i]);
 	std::cout << "\n";
